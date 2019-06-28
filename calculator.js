abc449552d4e300035743545f2fe3d8ce2b79651
cindychea <cindy_chea@hotmail.com>
2019-06-27 23:19:36 -0400

    completed testing exercise

--- calculator.js
+++ calculator.js
@@ -1,10 +1,13 @@
-function add() {
+function add(a, b) {
+  return a + b;
 }
 
-function subtract() {
+function subtract(a, b) {
+  return a - b;
 }
 
-function sum() {
+function sum(n) {
+  return n.reduce((a, b) => a + b, 0)
 }
 
 module.exports = {add, subtract, sum}
